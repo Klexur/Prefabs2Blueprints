d5526b824ed9d33a1528760a1e99405f6e929aa6
Klexur <klexur@gmail.com>
2026-01-20 22:18:45 -0600

    Update config.py
    - Changed default values.

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 # This folder contains all the prefabs to convert
 # They will be put in a folder equal to their filename
-BATCH_PATH = "" # Use only forward slashes in the path
+# Use only forward slashes in the path
+# Default value is the game's Prefabs folder
+BATCH_PATH = "C:/Program Files (x86)/Steam/steamapps/common/SpaceEngineers/Content/Data/Prefabs"
 
 # The program will prepend this when creating the folder in batch mode, meaning it will add this in front of the name of the folder
-PREPEND = ""
+PREPEND = "PREFAB_"
 
 # The program will append this when creating the folder in batch mode, meaning it will add this to the end of the name of the folder
 APPEND = ""
